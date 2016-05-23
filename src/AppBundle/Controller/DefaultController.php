ba2a5896b2c4e4477f4600910fc60fb63fb5d9e4
Diego Nobre <dcnobre@gmail.com>
2016-05-23 00:09:59 -0300

    Entities, repositories, default controller and updated twig theme

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
+use AppBundle\Form\Type\PostType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,11 +13,66 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+
+        // total number of posts
+        $totalNumberOfPosts = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->getTotalNumberOfPosts();
+
+        $totalNumberOfViews = 0;
+        if ($totalNumberOfPosts > 0) {
+            $totalNumberOfViews = $this->getDoctrine()
+                ->getRepository('AppBundle:Analytics')
+                ->getUpdatedTotalNumberOfViews();
+        }
+
+        // list images
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository('AppBundle:Post')->findBy(array(), array('date' => 'desc'));
+
+        return $this->render('default/index.html.twig', array(
+            'form' => $form->createView(),
+            'posts' => $posts,
+            'totalNumberOfPosts' => $totalNumberOfPosts,
+            'totalNumberOfViews' => $totalNumberOfViews,
+        ));
+    }
+
+    /**
+     * @Route("/post", name="post")
+     * @param Request $request
+     */
+    public function postAction(Request $request)
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // $file stores the uploaded PDF file
+            $file = $post->getImgName();
+
+            // Generate a unique name for the file before saving it
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+
+            // Move the file to the directory where brochures are stored
+            $imagesDir = $this->container->getParameter('kernel.root_dir').'/../web/uploads/images';
+            $file->move($imagesDir, $fileName);
+
+            // Update the 'brochure' property to store the PDF file name
+            // instead of its contents
+            $post->setImgName($fileName);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('homepage'));
+        }
     }
 }
---
+++ src/AppBundle/Form/Type/PostType.php
@@ -0,0 +1,38 @@
+<?php
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->setAction('post')
+            ->setMethod('POST')
+            ->add('title', TextType::class, array(
+                'label' => false,
+                'attr'   =>  array(
+                    'placeholder'   => 'Do you wanna post with a title? Type here!',
+                    'data-min-file-count' => '1'
+                    ),
+                'required' => false
+                )
+            )
+            ->add('imgName', FileType::class, array(
+                'label' => false,
+                'attr'   =>  array(
+                    'class'   => 'file',
+                    'data-min-file-count' => '1'
+                    )
+                )
+            )
+            ->add('save', SubmitType::class, array('label' => 'Publish'))
+            ->getForm();
+        ;
+    }
+}
